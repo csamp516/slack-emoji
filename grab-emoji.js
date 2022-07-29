b2b7a903cea611c77ca54115f761985026970dc9
Chris Samperisi <csamperisi@everfi.com>
2022-07-29 14:30:07 -0700

    use filenames instead

--- grab-emoji.js
+++ grab-emoji.js
@@ -1,18 +1,26 @@
-const emojis = require('./emoji').emoji;
+if (!process.argv[2]) {
+  console.log('You forgot the filename!');
+  console.log('(╯°□°)╯︵ ┻━┻ ');
+  process.exit();
+}
+const emojis = require(`./${process.argv[2]}`).emoji;
 const fs = require('fs');
 const https = require('https');
-
+console.log('Emoji', emojis.length);
 for (emoji of emojis) {
-  console.log('emoji url', emoji.url);
   const extension = emoji.url.split('.').pop();
   const file = fs.createWriteStream(`./output/${emoji.name}.${extension}`);
-  const request = https.get(emoji.url, function (response) {
-    response.pipe(file);
+  const request = https
+    .get(emoji.url, function (response) {
+      response.pipe(file);
 
-    // after download completed close filestream
-    file.on('finish', () => {
-      file.close();
-      console.log('Download Completed');
+      // after download completed close filestream
+      file.on('finish', () => {
+        file.close();
+        // console.log('Download Completed');
+      });
+    })
+    .on('error', e => {
+      console.warn('error!', e);
     });
-  });
 }
